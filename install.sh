d6dc11b3f683868b4236b5a6d2dfde9f2e912ddc
entimosmagos <entimosmagos@proton.me>
2026-03-20 05:05:17 +0400

    add install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "installing grimoire..."
+
+DOTFILES="$(cd "$dirname "$0")" && pwd)"
+
+link() {
+  local src="$DOTFILES/$1"
+  local dst="$HOME/.config/$2"
+  mkdir -p "$(dirname "$dst")"
+  cp -r "$src" "$dst"
+  echo " copied $1 -> $dst"
+}
+
+link "hypr"              "hypr"
+link "waybar/config"     "waybar/config"
+link "waybar/style.css"  "waybar/style.css"
+link "kitty/kitty.conf"  "kitty/kitty.conf"
+link "wofi/style.css"    "wofi/style.css"
+link "mako/config"       "mako/config"
+link "fastfetch"         "fastfetch"
+link "fish/config.fish"  "fish/config.fish"
+link "yazi/theme.toml"   "yazi/theme.toml"
+
+echo "done. reload hyprland with: hyprctl reload"
